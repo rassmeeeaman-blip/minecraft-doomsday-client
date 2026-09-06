63da702423a9fe2de23833d9edad2b2130a60228
rassmeeeaman-blip <rassmeeeaman@gmail.com>
2026-09-05 22:48:18 -0700

    Fix keybind handler and add logging

--- src/main/java/com/ahh/AhhClientEvents.java
+++ src/main/java/com/ahh/AhhClientEvents.java
@@ -3,19 +3,23 @@ package com.ahh;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.Environment;
 import net.fabricmc.api.EnvType;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import com.ahh.module.ModuleManager;
 import com.ahh.ui.GuiManager;
 import com.ahh.anticheat.AntiCheatBypass;
+import net.minecraft.client.MinecraftClient;
+import org.lwjgl.glfw.GLFW;
 
 @Environment(EnvType.CLIENT)
 public class AhhClientEvents implements ClientModInitializer {
     public static ModuleManager moduleManager;
     public static GuiManager guiManager;
     public static AntiCheatBypass antiCheat;
+    private static boolean lastShiftState = false;
 
     @Override
     public void onInitializeClient() {
-        AhhClient.LOGGER.info("Ahh Client client events initialized!");
+        AhhClient.LOGGER.info("Ahh Client initialized!");
         
         // Initialize managers
         moduleManager = new ModuleManager();
@@ -25,6 +29,23 @@ public class AhhClientEvents implements ClientModInitializer {
         // Register all modules
         moduleManager.registerModules();
         
-        AhhClient.LOGGER.info("Anti-Cheat Bypass system enabled!");
+        AhhClient.LOGGER.info("Ahh Client modules loaded: " + moduleManager.getModules().size());
+        
+        // Register keybind handler
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            if (client.player != null && client.currentScreen == null) {
+                boolean shiftPressed = GLFW.glfwGetKey(client.getWindow().getHandle(), GLFW.GLFW_KEY_LEFT_SHIFT) == GLFW.GLFW_PRESS;
+                
+                // Detect shift press (transition from false to true)
+                if (shiftPressed && !lastShiftState) {
+                    AhhClient.LOGGER.info("LEFT SHIFT pressed - Opening GUI");
+                    guiManager.toggleGui();
+                }
+                
+                lastShiftState = shiftPressed;
+            } else {
+                lastShiftState = false;
+            }
+        });
     }
 }
